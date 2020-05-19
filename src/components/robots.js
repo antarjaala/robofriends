4b6eee9edbdc192c0e9bedd5addb188f798416e9
ShivaprasadKyathanatty <shivaprasadsk@gmail.com>
2020-05-19 13:13:32 +0530

    adding files first time

---
+++ src/components/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Sannidhi',
+      username: 'Bret',
+      email: 'Sannidhi@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Shruti',
+      username: 'Antonette',
+      email: 'Shruti@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Alexa',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Katie',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Dishika',
+      username: 'Kamren',
+      email: 'Dishika@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Peppa',
+      username: 'Leopoldo_Corkery',
+      email: 'Peppa@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Vernika',
+      username: 'Elwyn.Skiles',
+      email: 'Vernika@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Aanya',
+      username: 'Maxime_Nienow',
+      email: 'Aanya@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Arnav',
+      username: 'Delphine',
+      email: 'Arnav@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Siri',
+      username: 'Moriah.Stanton',
+      email: 'Siri@karina.biz'
+    }
+  ];
+  
